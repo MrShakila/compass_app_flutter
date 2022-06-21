1b6ff3f7f4eb1339889523826d1b20b836247a60
MrShakila <dscshakila@gmail.com>
2022-06-21 19:20:06 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/compass_app_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.compass_app_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screen/home_screen.dart
@@ -0,0 +1,72 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_compass/flutter_compass.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  double? heading = 0;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    FlutterCompass.events!.listen((event) {
+      setState(() {
+        heading = event.heading;
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.green,
+        appBar: AppBar(
+          elevation: 0,
+          centerTitle: true,
+          backgroundColor: Colors.teal,
+          title: const Text("Compass App"),
+        ),
+        body: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                "${heading!.ceil()}",
+                style: const TextStyle(
+                    color: Colors.white,
+                    fontSize: 26.0,
+                    fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(
+                height: 50,
+              ),
+              Center(
+                child: Stack(
+                  alignment: Alignment.center,
+                  children: [
+                    const SizedBox(
+                      child: Icon(
+                        Icons.circle,
+                        size: 200,
+                      ),
+                    ),
+                    Transform.rotate(
+                      angle: (heading ?? 0) * (pi / 180) * -1,
+                      child: Image.asset("lib/images/1.png"),
+                    )
+                  ],
+                ),
+              )
+            ]),
+      ),
+    );
+  }
+}
